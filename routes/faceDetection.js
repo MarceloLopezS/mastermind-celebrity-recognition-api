39c545dbc5d21b7df76cfeabe2c654ec63799230
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2023-03-01 15:40:30 -0500

    Implement connection and auth for Clarifai API
    Connection now allows to send an image URL to Clarifai API and recieve
    a response containing the data from it's celebrity-face-detection IA
    model.
    
    The important data is sent to the front-end (boundingBox coordinates,
    name and correct detection probability).
    
    IMPORTANT: Local development doesn't allow to serve images from the
    local server, so the image URL for the request is hard coded. Must
    change to a dinamic URL (already done but commented out) once the
    server is online.
    All uploading images and serving them logic is done.

--- routes/faceDetection.js
+++ routes/faceDetection.js
@@ -1,4 +1,7 @@
 import fs from 'fs';
+import { ClarifaiStub, grpc } from 'clarifai-nodejs-grpc';
+
+const domain = "https://soft-humans-return-179-6-54-221.loca.lt";
 
 const faceDetection = (req, res, next) => {
     if (!req.authorizedUser) {
@@ -11,10 +14,68 @@ const faceDetection = (req, res, next) => {
     }
 
     // Clarifai logic
-    console.log(req?.file);
-    return res.status(200).json({
-        status: 'success'
-    })
+    // console.log(req?.file);
+    const imageFolder = req.file.destination.split("/").at(-1);
+    // const imageURL = `${domain}/${imageFolder}/${req.file.filename}`;
+    const imageURL = "https://occ-0-41-300.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABbFI2wcwiGkHDdGWaw58hWgLETOBsbqqv6GbKnZFn3s_Y4fjw0Ys9DNYD5txnfV3oj9tgsBeaSnPcBOwQqQnpHVqHeQr9FtvVzaL.jpg?r=776";
+    // console.log(imageURL);
+
+    const stub = ClarifaiStub.grpc();
+    const metadata = new grpc.Metadata();
+    metadata.set("authorization", `Key ${process.env.PAT}`);
+
+    stub.PostModelOutputs(
+        {
+            user_app_id: {
+                "user_id": process.env.USER_ID,
+                "app_id": process.env.APP_ID
+            },
+            model_id: process.env.MODEL_ID,
+            inputs: [
+                { data: { image: { url:  imageURL } } }
+            ]
+        },
+        metadata,
+        (err, response) => {
+            fs.unlink(req.file.path, err => {
+                if (err) console.log(err);
+            });
+            
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    status: 'fail'
+                })
+            }
+
+            if (response.status.code !== 10000) {
+                console.log("Post model outputs failed: " + response.outputs[0].status.description);
+                console.log("Details: " + response.outputs[0].status.details);
+                return res.status(500).json({
+                    status: 'fail'
+                })
+            }
+
+            const regions = response.outputs[0].data.regions;
+            
+            const detectionData = regions.map((region) => {
+                const boundingBox = region.region_info.bounding_box;
+                const faceDetection = {
+                    name: region.data.concepts[0].name,
+                    probability: region.data.concepts[0].value
+                }
+                return {
+                    boundingBox,
+                    faceDetection
+                }
+            })
+
+            return res.status(200).json({
+                status: 'success',
+                detectionData
+            })
+        }
+    )
 }
 
 export default faceDetection;
--- routes/incrementEntry.js
+++ routes/incrementEntry.js
@@ -1,4 +1,7 @@
-const incrementEntry = (req, res, next) => {
+import db from '../database/db.js';
+import jwt from 'jsonwebtoken';
+
+const incrementEntry = async (req, res, next) => {
     if (!req.authorizedUser) {
         return res.status(403).json({
             status: 'unauthorized'
@@ -6,6 +9,28 @@ const incrementEntry = (req, res, next) => {
     }
 
     // Increment user entries
+    const userEmail = req.authorizedUser;
+    const selectUserQuery = "SELECT * FROM users WHERE email = $1";
+    const selectUserValues = [userEmail];
+
+    const selectUserResponse = await db.query(selectUserQuery, selectUserValues);
+    if (selectUserResponse.rowCount === 0) {
+        return res.status(500).json({
+            status: 'fail'
+        })
+    }
+
+    const newUserEntries = selectUserResponse.rows[0].entries + 1;
+    const updateUserQuery = "UPDATE users SET entries = $1 WHERE email = $2";
+    const updateUserValues = [newUserEntries, userEmail];
+
+    const updateUserResponse = await db.query(updateUserQuery, updateUserValues);
+    if (updateUserResponse.rowCount === 0) {
+        return res.status(500).json({
+            status: 'fail'
+        })
+    }
+
     return res.status(200).json({
         status: 'success'
     })
