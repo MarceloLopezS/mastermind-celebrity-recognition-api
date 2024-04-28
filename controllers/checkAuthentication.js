3cfe5394978f0dd6f672069116de27e47a9670bb
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-04-27 22:38:06 -0500

    Add: checkAuthentication controller

---
+++ controllers/checkAuthentication.js
@@ -0,0 +1,11 @@
+const checkUserAuthentication = (req, res) => {
+  if (!req.authorizedUser) return res.status(403).json({
+    authenticated: false
+  })
+
+  return res.status(200).json({
+    authenticated: true
+  })
+}
+
+export default checkUserAuthentication
