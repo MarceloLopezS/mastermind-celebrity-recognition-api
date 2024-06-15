0a417f41d30a9035e98f9134708aba1127e8c319
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-06-15 13:57:03 -0500

    Add: account model in adapters layer

---
+++ adapters/models/account.js
@@ -0,0 +1,56 @@
+import { stringDateToTimestamp } from "../../utilities/functions.js"
+
+class AccountModel {
+  #db
+
+  constructor(db) {
+    this.#db = db
+  }
+
+  activate = async account => {
+    const selectAuth = "SELECT * FROM auth WHERE email = $1"
+    const selectAuthValues = [account.email]
+
+    const selectAuthResponse = await this.#db.query(
+      selectAuth,
+      selectAuthValues
+    )
+    if (selectAuthResponse.rowCount === 0) {
+      throw Error("Email not found.")
+    }
+
+    if (
+      selectAuthResponse.rows[0].expiration !== "infinity" &&
+      stringDateToTimestamp(selectAuthResponse.rows[0].expiration) <
+      Date.now()
+    ) {
+      throw Error("Activation link expired.")
+    }
+
+    if (selectAuthResponse.rows[0].activation === "active") {
+      return { status: "success" }
+    }
+
+    account.setActivation("active")
+    account.setExpiration(Infinity)
+    const updateAuth =
+      "UPDATE auth SET activation = $1, expiration = $2 WHERE email = $3"
+    const updateAuthValues = [
+      account.activation,
+      account.expiration,
+      account.email
+    ]
+
+    const updateAuthResponse = await this.#db.query(
+      updateAuth,
+      updateAuthValues
+    )
+    if (updateAuthResponse.rowCount === 0) {
+      throw Error("Failed to update database.")
+    }
+
+    return { status: "success" }
+  }
+}
+
+export default AccountModel
