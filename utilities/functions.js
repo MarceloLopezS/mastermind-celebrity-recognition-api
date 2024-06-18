1d3446019590eae1c5941d9b3c45d44aa2bad0bd
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-06-18 12:48:04 -0500

    Update: Add isValidEmail function

--- utilities/functions.js
+++ utilities/functions.js
@@ -1,3 +1,13 @@
 export const stringDateToTimestamp = stringDate => {
   return new Date(stringDate).getTime()
 }
+
+export const isValidEmail = (string) => {
+  if (typeof string !== "string") {
+    throw new TypeError("Email must be a string.")
+  }
+
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+  return string.match(emailRegex)
+}
