fe216d42715c24424adf6223e9bf10c0cf484b9b
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-06-22 16:43:21 -0500

    Add: DetectionModel

--- adapters/models/detection.js
+++ adapters/models/detection.js
@@ -26,3 +26,68 @@ export class DemoDetectionModel {
     }
   }
 }
+
+export class DetectionModel {
+  #stub
+  #grpc
+
+  constructor(ClarifaiStub, grpc) {
+    this.#stub = ClarifaiStub
+    this.#grpc = grpc
+  }
+
+  getData = async (imageUrl, Detection) => {
+
+    const stub = this.#stub.grpc()
+    const metadata = new this.#grpc.Metadata()
+
+    metadata.set("authorization", `Key ${process.env.PAT}`)
+    const dataPromise = new Promise((resolve, reject) => {
+      stub.PostModelOutputs(
+        {
+          user_app_id: {
+            user_id: process.env.USER_ID,
+            app_id: process.env.APP_ID
+          },
+          model_id: process.env.MODEL_ID,
+          inputs: [{ data: { image: { url: imageUrl } } }]
+        },
+        metadata,
+        (err, response) => {
+          if (err) {
+            return reject(new Error(err))
+          }
+
+          if (response.status.code !== 10000) {
+            return reject(new Error(
+              "Post model outputs failed: "
+              + response.outputs[0].status.description
+              + "\n"
+              + response.outputs[0].status.details
+            ))
+          }
+
+          const rawRegions = response.outputs[0].data
+
+          const regions = rawRegions.map(region => {
+            const detection = new Detection()
+            const boundingBox = region.region_info.bounding_box
+            const faceDetection = {
+              name: region.data.concepts[0].name,
+              probability: region.data.concepts[0].value
+            }
+
+            detection.setBoundingBox(boundingBox)
+            detection.setFaceDetection(faceDetection)
+
+            return detection.data
+          })
+
+          resolve({ status: "success", statusCode: 200, regions })
+        }
+      )
+    })
+
+    return await dataPromise
+  }
+}
