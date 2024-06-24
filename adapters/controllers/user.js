74aa0404c48778c11dde410c735fa467405dfc4f
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-06-24 17:28:14 -0500

    Add: UserEntriesController

--- adapters/controllers/user.js
+++ adapters/controllers/user.js
@@ -34,3 +34,42 @@ export class UserInfoController {
     }
   }
 }
+
+export class UserEntriesController {
+  #incrementDetectionEntry
+
+  constructor(incrementDetectionEntryHandler) {
+    this.#incrementDetectionEntry = incrementDetectionEntryHandler
+  }
+
+  incrementDetectionEntry = async (req, res) => {
+    if (!req.authorizedUser) {
+      return res.status(403).json({
+        status: "unauthorized"
+      })
+    }
+
+    try {
+      const email = req.authorizedUser
+      const result = await this.#incrementDetectionEntry({ email })
+
+      if (result.status !== "success") {
+        res.status(result.statusCode).json({
+          status: result.status
+        })
+      } else {
+        res.status(result.statusCode).json({
+          status: result.status
+        })
+      }
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({
+        status: "fail",
+        fail: {
+          message: "There was a problem incrementing user entries."
+        }
+      })
+    }
+  }
+}
